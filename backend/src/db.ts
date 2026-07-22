abe887cd76d92d9cbe2423061d5563345e784580
MarcuSS002 <hyperxaxis@gmail.com>
2026-07-22 11:13:02 +0530

    fix: correct prisma import path, add type module, move devDeps

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 
 console.log("DATABASE_URL:", process.env.DATABASE_URL);
 
-import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaClient } from "./generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
